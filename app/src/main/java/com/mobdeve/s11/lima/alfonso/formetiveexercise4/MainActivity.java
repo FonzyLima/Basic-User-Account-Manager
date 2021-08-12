d1f926782cf550ed40aca38d795a13fc0533eda6
FonzyLima <71457986+FonzyLima@users.noreply.github.com>
2021-08-12 11:54:51 +0800

    added files

---
+++ app/src/main/java/com/mobdeve/s11/lima/alfonso/formetiveexercise4/MainActivity.java
@@ -0,0 +1,14 @@
+package com.mobdeve.s11.lima.alfonso.formetiveexercise4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
